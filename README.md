aaf43cc5469fe201fc8a652d52f8b1793fb294c5
Rami Mohammed <seidramimohammed@gmail.com>
2021-03-31 06:34:23 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,56 @@
+#HotPress 🔥 - Hot Reloading for ExpressJs
+##Philosophy
+- Why should you restart a http server just for a small change in your source code. isn't HTTP meant to be stateless.
+- Aren't you meant to have a static fetch-once properties for development.
+
+If you agree with those philosophies, then you'll love HotPress. 
+
+What HotPress does is that it only initializes the server and required parameters once and pass it to your virtual server (Router).
+
+##Usage
+```shell
+npm install @soniccodes/hotpress
+```
+
+####./foo.js - The reloadable file
+```js
+/**
+  @param server : Router
+**/
+function reload(server) {
+   server.get("/hi",function(req, res){
+       res.send("hi")
+   })
+   //Anything you'd do with a normal server.  
+}
+modules.export = reload //This is very important
+```
+
+####server.js 
+```js
+const express = require("express")
+const {setup,reload} = require("@soniccodes/hotpress")
+const app = express()
+setup({server:app,reloadable:"./foo.js"})
+app.listen(port,(err)=>{
+   if(err) throw err;
+   console.log("⚡ Ready on http://localhost:"+port);
+})
+```
+
+##Here's what you should expect.
+###Before
+
+###After
+
+##License
+```
+Copyright 2021 Rami Mohammed
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+```
+
---
+++ index.js
@@ -0,0 +1,78 @@
+const {Express} = require("express")
+const {
+    performance
+} = require('perf_hooks');
+const fs = require("fs")
+const express = require("express");
+let router = null;
+let wasError = false;
+let reload = function() {
+    throw "You haven't initialized HotPress yet.";
+}
+
+
+/**
+ * @param {Object} config
+ * @param {string} config.reloadable - the file that's going to be hot reloaded (i.e ./foo.js).
+ * @param {Express} config.server - a pre initialized server
+ * @param {function():void} config.preload - to let you do things before loading the app, i.e (clear mongoose models)
+ * @param {function():void} config.postload - to let you do things after the app is successfully loaded.
+ * @param {function(e:Error):void} config.error - to notify you of an error with what happened.
+ * @param {any} params - parameters that'll be passed to your initializer.
+ *
+ * - ./foo.js - your hot reloadable server.
+ *  ```
+ *  /**
+ *  \@param server : Router
+ *  **\/
+ *  function reload(server, ...params) {
+ *      // Anything you do normally
+ *  }
+ *  modules.export = reload
+ *  ```
+ *
+ *  - ./server.js - your good old friend.
+ *  ```
+ *  const express = require("express")
+ *  const {setup,reload} = require("@soniccodes/hotpress")
+ *  const app = express()
+ *  setup("./foo.js",app)
+ *  app.listen(**)
+ *  ```
+ */
+function setup(config, ...params){
+    reload = function() {
+        const start = performance.now();
+        try{
+            if(config.preload != null) config.preload()
+            router = null;
+            router = express.Router();
+            delete require.cache[require.resolve(config.reloadable)]
+            const reloaded = require(config.reloadable)
+            reloaded(router,...params)
+            const end = performance.now();
+            if(wasError){
+                console.log(`🎉 The last error was recovered in ${Math.round(end - start)}ms..`)
+            }else{
+                console.log(`⚡ Reloaded your server in ${Math.round(end - start)}ms.`)
+            }
+            wasError = false;
+            if(config.postload != null) config.postload()
+        }catch (e) {
+            wasError = true;
+            const end = performance.now();
+            console.log(`💀 Something went wrong in ${Math.round(end - start)}ms.`)
+            console.log(e.stack)
+            if(config.error != null) config.error(e)
+        }
+    }
+    fs.watchFile(config.reloadable,{interval:100},()=>{
+        reload();
+    })
+    reload()
+}
+
+module.exports = {setup, reload}
+
+
+
